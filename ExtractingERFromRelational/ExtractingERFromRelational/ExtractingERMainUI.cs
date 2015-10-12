6f52fb29f2d2209d91266cb633747cb8da38c5b8
Kaushik Santhanam <doors2kaushik@gmail.com>
2015-10-12 08:25:39 -0700

    Renamed Solution Files and Added Business Logic

--- ExtractingERFromRelational/ExtractingERFromRelational/ExtractingERMainUI.cs
+++ ExtractingERFromRelational/ExtractingERFromRelational/ExtractingERMainUI.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ExtractingERFromRelational
 {
-    public partial class Form1 : Form
+    public partial class ExtractingERMainUI : Form
     {
-        public Form1()
+        public ExtractingERMainUI()
         {
             InitializeComponent();
         }
