731bca674596fabf7ec149ffdf57d2cb47adc7d6
Kaushik Santhanam <doors2kaushik@gmail.com>
2015-10-29 16:31:19 -0400

    Main PAge CHnage

--- ExtractingERFromRelational/ExtractingERFromRelational/ExtractingERMainUI.Designer.cs
+++ ExtractingERFromRelational/ExtractingERFromRelational/ExtractingERMainUI.Designer.cs
@@ -30,10 +30,11 @@
         {
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.addRelationButton = new System.Windows.Forms.ToolStripButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.relationsDataGrid = new System.Windows.Forms.DataGridView();
             this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.relationsDataGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -43,7 +44,7 @@
             this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(458, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(816, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -58,40 +59,32 @@
             this.addRelationButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.addRelationButton.Click += new System.EventHandler(this.addRelationButton_Click);
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Relations";
+            this.panel1.Controls.Add(this.relationsDataGrid);
+            this.panel1.Location = new System.Drawing.Point(12, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 212);
+            this.panel1.TabIndex = 1;
             // 
-            // label2
+            // relationsDataGrid
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(147, 43);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Selected Relation Details";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 59);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 212);
-            this.listBox1.TabIndex = 4;
+            this.relationsDataGrid.AllowUserToAddRows = false;
+            this.relationsDataGrid.AllowUserToDeleteRows = false;
+            this.relationsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.relationsDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.relationsDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.relationsDataGrid.Name = "relationsDataGrid";
+            this.relationsDataGrid.ReadOnly = true;
+            this.relationsDataGrid.Size = new System.Drawing.Size(791, 212);
+            this.relationsDataGrid.TabIndex = 0;
             // 
             // ExtractingERMainUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 362);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(816, 537);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.toolStrip1);
             this.MaximizeBox = false;
             this.Name = "ExtractingERMainUI";
@@ -99,6 +92,8 @@
             this.Text = "ER-To-Relational-Model";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.relationsDataGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,9 +103,8 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton addRelationButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView relationsDataGrid;
     }
 }
 
--- ExtractingERFromRelational/ExtractingERFromRelational/ExtractingERMainUI.cs
+++ ExtractingERFromRelational/ExtractingERFromRelational/ExtractingERMainUI.cs
@@ -1,32 +1,87 @@
 ﻿using ExtractingERBusinessLogic;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ExtractingERFromRelational
 {
     public partial class ExtractingERMainUI : Form
     {
+        #region Variables
 
+        /// <summary>
+        /// Contains the list of Relations in added by the user.
+        /// </summary>
         public static List<Relation> _masterListRelations;
 
+        public const string relNameColumnString = "RelationName";
+        public const string primaryKeyColumnString = "PrimaryKey";
+        public const string attributesColumnString = "Attributes";
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public ExtractingERMainUI()
         {
             InitializeComponent();
             _masterListRelations = new List<Relation>();
+
+            // Initializes Columns of the DataGridView
+            relationsDataGrid.Columns.Add(relNameColumnString, "RELATION NAME");
+            relationsDataGrid.Columns.Add(primaryKeyColumnString, "PRIMARY KEY");
+            relationsDataGrid.Columns.Add(attributesColumnString, "ATTRIBUTES");
         }
 
+        #endregion
+
+        #region Private Helper Methods
+        
+        /// <summary>
+        /// Refreshes the data grid of relations with any new 
+        /// relations which were added by the user.
+        /// </summary>
+        private void RefreshRelationsDataGrid()
+        {
+            relationsDataGrid.Rows.Clear();
+            foreach(Relation rel in _masterListRelations)
+            {
+                string attributeConcat = string.Empty;
+
+                // Concatenate the attribute list to a comma seperated string
+                foreach(KeyValuePair<string, AttributeType> kvp in rel.attributeDict)
+                {
+                    attributeConcat += kvp.Key + ",";
+
+                }
+
+
+                relationsDataGrid.Rows.Add(rel.relationName, rel.primaryKey, attributeConcat);
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Event Handler which executes Opening up a 
+        /// new relation form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void addRelationButton_Click(object sender, EventArgs e)
         {
             AddRelationForm newRelation = new AddRelationForm();
             newRelation.ShowDialog();
+
+            // Refresh the list of Relations added to the master list
+            RefreshRelationsDataGrid();
         }
 
+
+        #endregion
     }
 }
