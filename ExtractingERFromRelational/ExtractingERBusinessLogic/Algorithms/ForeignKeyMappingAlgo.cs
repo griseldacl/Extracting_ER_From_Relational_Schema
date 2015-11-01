6a8175c63da2487a8d43f7b14ec1009d7130aa66
Kaushik Santhanam <doors2kaushik@gmail.com>
2015-11-01 15:12:23 -0500

    Placeholder for Algo method

--- ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/ForeignKeyMappingAlgo.cs
+++ ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/ForeignKeyMappingAlgo.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace ExtractingERBusinessLogic.Algorithms
 {
-    public class ForeignKeyMappingAlgo
-    {
+    public static class ForeignKeyMappingAlgo
+    { 
+        #region Public Static Methods
+
+        public static List<Tuple<string,string>> CalculateForeignKeyRelations(List<Relation> masterListRelation)
+        {
+            List<Tuple<string, string>> foreignKeyRelations = new List<Tuple<string, string>>();
+            // Add code here to calculate the foreign key dependencies
+
+
+            return foreignKeyRelations;
+        }
+
+        #endregion
     }
 }
