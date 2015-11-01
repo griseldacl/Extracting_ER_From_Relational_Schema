040df9bf9715b4d8325a45487eac405e32b48fff
Frederic Liu <eypw152@outlook.com>
2015-11-01 17:30:15 -0500

    ForeignKeyMappingAlgo

--- ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/ForeignKeyMappingAlgo.cs
+++ ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/ForeignKeyMappingAlgo.cs
@@ -10,11 +10,30 @@ namespace ExtractingERBusinessLogic.Algorithms
     { 
         #region Public Static Methods
 
-        public static List<Tuple<string,string>> CalculateForeignKeyRelations(List<Relation> masterListRelation)
+        public static List<Tuple<string,string,List<string>>> CalculateForeignKeyRelations(List<Relation> masterListRelation)
         {
-            List<Tuple<string, string>> foreignKeyRelations = new List<Tuple<string, string>>();
+            List<Tuple<string, string,List<string>>> foreignKeyRelations = new List<Tuple<string, string,List<string>>>();
             // Add code here to calculate the foreign key dependencies
+            foreach(Relation rel in masterListRelation)
+            {
+                foreach(KeyValuePair<string,AttributeType> kp1 in rel.attributeDict)
+                {
+                    for (int id = masterListRelation.IndexOf(rel) + 1, id < masterListRelation.Count, id++)
+                    {
+                        foreach(KeyValuePair<string,AttributeType> kp2 in masterListRelation.ElementAt(id).attributeDict)
+                        {
+                            if (kp1.Equals(kp2))
+                            {
+                                if (masterListRelation.ElementAt(id).primaryKey.Contains(kp2))
+                                    foreignKeyRelations.Add(new Tuple<string, string, List<string>>(rel.relationName, masterListRelation.ElementAt(id),
+                                        new List<string>(kp1.Key)));
+                            }
+                        }
 
+                    }
+                }
+                
+            }
 
             return foreignKeyRelations;
         }
