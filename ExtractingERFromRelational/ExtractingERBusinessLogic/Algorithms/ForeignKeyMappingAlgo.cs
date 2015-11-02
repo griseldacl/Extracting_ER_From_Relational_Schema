50c9779be15402fe90a43e073fc3ca49ebbba37c
griseldacl <g.conejolopez@gmail.com>
2015-11-01 20:14:52 -0500

    SECOND CHANGES, I added the foor loop to compare the primary key against the attributes of the other attributes.. pending tryind to add the names in the foreignkey function

--- ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/ForeignKeyMappingAlgo.cs
+++ ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/ForeignKeyMappingAlgo.cs
@@ -4,27 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ExtractingERBusinessLogic.Algorithms
 {
     public static class ForeignKeyMappingAlgo
-    { 
+    {
         #region Public Static Methods
-
+        
         public static List<Tuple<string,string>> CalculateForeignKeyRelations(List<Relation> masterListRelation)
         {
             List<Tuple<string, string>> foreignKeyRelations = new List<Tuple<string, string>>();
             // Add code here to calculate the foreign key dependencies 
 
 
-            int index = 1;
-            int count = masterListRelation.Count();
-            string pk = masterListRelation.ElementAt(1).primaryKey;
+            //not working: It is skipping line string PK and it is not entering the foreach loop
+            //string PK = null;
+            //foreach (Relation rel in masterListRelation)
+            //{
+            //    for (int i = 0; i <= masterListRelation.Count(); i++)
+            //    {
+            //        PK = masterListRelation.ElementAt(i).primaryKey;   //PK is not being updated
+            //    }        
+            //}
 
-
-            foreach (Relation rel in masterListRelation)
+            //working
+            string[] arr = new string[masterListRelation.Count()];
+            
+            for (int i = 0; i <= masterListRelation.Count(); i++)
             {
-                
-                
+                arr[i] = masterListRelation.ElementAt(i).primaryKey;
+
+                for (int j = 1; j <= masterListRelation.Count(); j++)
+                {
+                    if (masterListRelation.ElementAt(j).attributeDict.ContainsKey(arr[i]))
+                    {
+                        foreignKeyRelations.Add(masterListRelation.ElementAt(i).relationName, masterListRelation.ElementAt(j).relationName, arr[i]);
+                    }
+                }
+
             }
 
             return foreignKeyRelations;
