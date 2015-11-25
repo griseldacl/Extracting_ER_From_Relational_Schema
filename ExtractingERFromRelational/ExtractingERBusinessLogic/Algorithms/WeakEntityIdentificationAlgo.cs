63016929ff5bc568cfb6121bdec3a907e68ae082
Kaushik Santhanam <doors2kaushik@gmail.com>
2015-11-25 15:57:25 -0500

    Weak Entity Algorithm Committ

--- ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/WeakEntityIdentificationAlgo.cs
+++ ExtractingERFromRelational/ExtractingERBusinessLogic/Algorithms/WeakEntityIdentificationAlgo.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace ExtractingERBusinessLogic.Algorithms
 {
-    public class WeakEntityIdentificationAlgo
+    public static class WeakEntityIdentificationAlgo
     {
+        public static List<Tuple<string, string>> CalculateWeakEntities(List<Relation> masterList)
+        {
+            List<Tuple<string, string>> weakEntities = new List<Tuple<string, string>>();
+
+            for (int outerIndex = 0; outerIndex < masterList.Count; outerIndex++)
+            {
+                for (int innerIndex = 0; innerIndex < masterList.Count; innerIndex++)
+                {
+                    if (innerIndex != outerIndex)
+                    {
+                        Relation outerRel = masterList.ElementAt(outerIndex);
+                        Relation innerRel = masterList.ElementAt(innerIndex);
+
+                        if (innerRel.primaryKeys.Count < outerRel.primaryKeys.Count)
+                        {
+                            bool weakEntityIdentifier = true;
+                            foreach (string primaryKey in innerRel.primaryKeys)
+                            {
+                                if (outerRel.primaryKeys.Contains(primaryKey) == false)
+                                {
+                                    weakEntityIdentifier = false;
+                                }
+
+                            }
+                            if (weakEntityIdentifier)
+                            {
+                                Tuple<string, string> weakEntity = new Tuple<string, string>(outerRel.relationName, innerRel.relationName);
+                                weakEntities.Add(weakEntity);
+                            }
+                        }
+                    }
+
+
+                }
+            }
+
+
+            return weakEntities;
+        }
     }
+
 }
